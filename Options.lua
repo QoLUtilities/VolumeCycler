3b837cb61dd93d49fa6113be0609817eaf36a05f
shane mcdonagh <87150478+QoLUtilities@users.noreply.github.com>
2021-07-10 00:48:12 -0700

    adding parse function

--- Options.lua
+++ Options.lua
@@ -7,3 +7,16 @@ function VC.CreateConfig()
 	local toonVCLabel = opt.CreateLabel(scrollchild, vcHeader, indent, -itemGap, QOLUtilsCommon.Labels.VC.Levels)
 	storage.VC.EditBoxLevels = opt.CreateEditBox(scrollchild, toonVCLabel, indent, -10, QOLUtilsCommon.TableToStr(config.VC.Levels), opt.ParseVolumeLevels)
 end
+
+function VC.ParseVolumeLevels(self)
+	local configLevels = self == common.Acct.VC.EditBoxLevels and QOL_Config_Acct.VC.Levels or QOL_Config_Toon.VC.Levels
+	local enteredPresets = common.StrToTable(self:GetText(), common.Patterns.Numbers)
+	local validPresets = {}
+	for i = 1, table.getn(enteredPresets) do
+		if common.VC.ValidLevel(enteredPresets[i]) then
+			table.insert(validPresets, enteredPresets[i])
+		end
+	end
+	configLevels = validPresets
+	self:SetText(common.TableToStr(configLevels))
+end
